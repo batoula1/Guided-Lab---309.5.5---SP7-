aaa706970cc2f90835ae14e60d5ffe3e7715c20e
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-28 01:00:12 -0400

    Finish

--- src/test/java/DemoApplicationTest.java
+++ src/test/java/DemoApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RequestParamAnnotationApplicationTests {
+class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
