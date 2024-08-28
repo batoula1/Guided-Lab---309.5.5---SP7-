dd90997d2e27b91c5286ca8df33e43e43ac1fa42
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-28 00:34:03 -0400

    MyProject

---
+++ src/main/java/com/Glab30955/RequestParam/annotation/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.Glab30955.RequestParam.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Glab30955/RequestParam/annotation/RequestParamAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Glab30955.RequestParam.annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestParamAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
